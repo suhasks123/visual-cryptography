ac077b4a46cd48e932f791f4fc3853ada6996724
adityasohoni <adityasohoni@gmail.com>
2021-03-19 19:19:42 +0530

    Added authenticate()

--- account.py
+++ account.py
@@ -1,3 +1,7 @@
+from PIL import Image
+import imagehash
+# hash = imagehash.average_hash(Image.open('test.png'))
+# print(hash)
 from typing import *
 
 class User:
@@ -20,12 +24,57 @@ class SharedAccount:
         self.balance = balance
         self.stakeholders = stakeholders
 
+
     def credit(self, request: Dict):
         print("credit")
 
     def debit(self, request: Dict):
-        print("debit")
+        # self.authenticate(Dict.uid)
+        #
+        # print("debit")
     
     def view_balance(self, request: Dict):
         print("view_balance")
+
+    def get_partial_image(self, user: User):
+        #Use socket to get partial image
+        #return it
+
+    def get_approval(self, user):
+
+
+    def check_hash(self, img_hash, user):
+        return img_hash == user.img_hash
+
+    def authenticate(self, uid):
+        """
+        Requests for the partial images from each user and receives them
+        After receiveing the images, it checks the hash for all the images
+        :return:
+        """
+        # i = 0
+        # for user in self.stakeholders:
+        #     i = i + 1
+        user = self.stakeholders[uid]
+        img = self.get_partial_image(user)
+        img_hash = imagehash.average_hash(img)
+        if self.check_hash(img_hash, user) == 0:
+            raise Exception("The Hash From user dosent match")
+            return False
+        filename = "img" + str(uid)
+        img.save(filename)
+        #Check for all threads
+        #If all are done then combine()
+        while(allthreadsdone()==0):
+        combined_img = combine()
+
+        approved = self.get_approval(user)
+
+        return True
+
+
+
+
+
+
 
